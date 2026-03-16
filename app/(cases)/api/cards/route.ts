b40ded3192d0d2453f73aa6d8b3ce7282d8ea091
Ricardo Henrique <41335992+rhAlves42@users.noreply.github.com>
2026-03-16 19:25:28 -0300

    Debug/cms (#28)
    * debug cms
    
    * debug 2
    
    * keep debugin
    
    * deeebuuuug

--- app/(cases)/api/cards/route.ts
+++ app/(cases)/api/cards/route.ts
@@ -3,18 +3,22 @@ import { type NextRequest, NextResponse } from "next/server";
 import { CaseCard } from "@/types/case";
 import { formatCardsContent } from "lib/content-formatter/contentFormatter";
 import { queryCasesByHighlight } from "@/lib/notion/get-cases";
+import {getDatabase} from "@/lib/notion/connection";
 
 import { HttpStatusCode } from "@/utils/httpStatus";
 import { AppLogger } from "@/lib/logger/logger";
+import { DATABASES } from "@/lib/notion/types";
 
 export async function GET(_req: NextRequest) {
   try {
+    const retrievedDB = await getDatabase(DATABASES.CASES)
     const queryCasesResponse = await queryCasesByHighlight({
       pageSize: 2
     });
 
     const data = formatCardsContent(queryCasesResponse);
     console.info("GET /api/cards successful: " + data.length);
+    console.info("GET retrievedDB: " + retrievedDB);
 
     return NextResponse.json(data, {
       status: HttpStatusCode.OK
